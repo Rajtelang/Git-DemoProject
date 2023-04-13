be976c69a6c30265a78214b3893e491b57af6d24
Rajtelang <rajtelang05@gmail.com>
2023-04-13 23:38:17 +0530

    first commit

---
+++ src/com/demo/project/Test1.java
@@ -0,0 +1,11 @@
+package com.demo.project;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("This is the new java Project to learn Git");
+	}
+
+}
